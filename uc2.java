3c30857553a22152daf6bdf0f44adfc93cfea7b9
arjun17697 <arjun17697@gmail.com>
2020-09-24 21:28:21 +0530

    edit uc2

--- uc2.java
+++ uc2.java
@@ -1,7 +1,10 @@
 public class uc2{
 	public static void main(String[] args){
 	double die=Math.floor(Math.random()*10)%6;
+	int pos=0;
 	int no=(int)die+1;
 	System.out.println("Number on die is " +no);
+	pos=pos+no;
+	System.out.println("New position is :" +pos);
 	}
 }
